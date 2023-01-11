87148408c94e07afe248a2d90a2fb4b0ce5f9c58
danibano <banovetz.danielle@gmail.com>
2023-01-11 00:31:51 -0600

    display changes as the day goes on

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -2,9 +2,58 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-function createHourBlock(hour, hourFormat) {
+const BUISNESS_HOURS_OPENED = 9
+const BUISNESS_HOURS_CLOSED = 18
+const PAST = "past"
+const PRESENT = "present"
+const FUTURE = "future"
+let clock = dayjs()
+let currentHour = clock.hour()
+
+
+function setClock() {
+  setInterval(() => { //run the code every second
+    clock = clock.add(1, "second") // adds 1 second to the clock
+
+    if (clock.hour() !== currentHour) { // if the hour changed then run handleHourChange function
+      currentHour = clock.hour() // if the hour did change then we need to update the current hour globally
+      handleHourChange(clock.hour())
+    }
+  }, 1000)
+}
+
+function handleHourChange(hour) { // the point of this function is to handle the hour change and alternate the tense css classes 
+  $(".time-block").each(function() {
+    const timeBlockId = $(this).attr("id") //grabs the hour id
+    const timeBlockHour = timeBlockId.split("-")[1] //splits the id into an array and gets the hour string
+    const previousTimeTense = $(this).attr("class").find((className) => [ PAST, PRESENT, FUTURE].includes(className))
+    const timeTense = getTimeTense(Number(timeBlockHour), hour) //grabbing the string and turning it into a number
+    const hourChanged = !$(this).hasClass(timeTense) //the hour changed if the timeTense is not included in the class
+    
+    if (hourChanged) {
+      $(this).toggleClass(`${previousTimeTense} ${timeTense}`) //if the hour did change it's gonna remove the previousTimeTense value and add the timeTense value into the class
+    }
+  })
+}
+
+
+function getTimeTense(hour, currentHour) {
+  if (!currentHour) {
+    currentHour = dayjs().hour()
+  }
+  
+  if (hour < currentHour) {
+    return PAST
+  } else if (hour === currentHour) {
+    return PRESENT
+  } else if (hour > currentHour) {
+    return FUTURE
+  }
+}
+
+function createHourBlock(hour, hourFormat) { //this creates the hour blocks by passing in stuff
   return (`
-    <div id="hour-${hour}" class="row time-block past">
+    <div id="hour-${hour}" class="row time-block ${getTimeTense(hour)}">
       <div class="col-2 col-md-1 hour text-center py-3">${hourFormat}</div>
       <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
       <button class="btn saveBtn col-2 col-md-1" aria-label="save">
@@ -15,9 +64,9 @@ function createHourBlock(hour, hourFormat) {
 }
 
 function renderBuisnessHours() {
-  for (let i = 9; i < 18 ; i++) { // looping from 9-18 because those are the buisness hours in military time
-    const currentHour = dayjs().hour(i).format("ha") // dayjs to get the 12-hour format
-    $("#buisness-hours").append(createHourBlock(i, currentHour)) // displays the return value of the createHourBlock function
+  for (let i = BUISNESS_HOURS_OPENED; i < BUISNESS_HOURS_CLOSED ; i++) { // looping from 9-18 because those are the buisness hours in military time
+    const hourFormat = dayjs().hour(i).format("ha") // dayjs to get the 12-hour format
+    $("#buisness-hours").append(createHourBlock(i, hourFormat)) // displays the return value of the createHourBlock function
   }
 }
 
@@ -30,7 +79,7 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-
+  setClock()
   renderBuisnessHours()
 
   // TODO: Add code to apply the past, present, or future class to each time
@@ -42,6 +91,5 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
 });
